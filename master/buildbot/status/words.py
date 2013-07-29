7e352d12ee43ce0297ab4e4b53ad5ac2d3494faa
Kevin Funk <kevin.funk@kdab.com>
2013-07-29 14:17:17 +0200

    Handle in unicode in our shlex.split wrapper
    Needed for upcoming Jabber support
    
    Related to http://bugs.python.org/issue1170.

--- master/buildbot/status/words.py
+++ master/buildbot/status/words.py
@@ -16,6 +16,7 @@
 import random
 import re
 import shlex
+import sys
 
 from string import capitalize
 from string import join
@@ -257,7 +258,15 @@ class Contact(base.StatusReceiver):
         """Returns list of arguments parsed by shlex.split() or
         raise UsageError if failed"""
         try:
-            return shlex.split(args)
+            # see http://bugs.python.org/issue1170
+            # shlex only supports unicode input in Python 2.7.3+
+            if isinstance(args, unicode):
+                if sys.hexversion >= 0x020703F0:
+                    return shlex.split(args)
+                else:
+                    return shlex.split(str(args))
+            else:
+                return shlex.split(args)
         except ValueError, e:
             raise UsageError(e)
 
